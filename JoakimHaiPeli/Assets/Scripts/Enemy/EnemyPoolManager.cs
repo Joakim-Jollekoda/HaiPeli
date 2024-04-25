c1ddf4af6c18a8e15373f3e9c2918237724bd0b6
SnaksiXD <aleksin.roskat@gmail.com>
2024-04-25 18:17:40 +0300

    enemypoolmanager aloitettu

--- JoakimHaiPeli/Assets/Scripts/Enemy/EnemyPoolManager.cs
+++ JoakimHaiPeli/Assets/Scripts/Enemy/EnemyPoolManager.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class EnemyPoolManager : MonoBehaviour
 {
     public static EnemyPoolManager Instance;
+    
 
+      public GameObject enemyPrefab;
+  public int poolSize = 20;
+  private Queue<GameObject> enemypool = new Queue<GameObject>();
     void Awake(){
         Instance = this;
+        InitializePool();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+private void InitializePool()
+{
+  for(int i = 0; i < poolSize; i++){
+    GameObject newenemy = Instantiate(enemyPrefab);
+    newenemy.SetActive(false);
+    enemypool.Enqueue(newenemy);
+  }
+}
+    
+    public GameObject GetEnemy(){
+    if(enemypool.Count > 0)
+    { 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    GameObject enemy = enemypool.Dequeue();
+    enemy.SetActive(true);
+    return enemy;
+    }
+    else{
+    GameObject newenemy = Instantiate(enemyPrefab);
+      newenemy.SetActive(true);
+      return newenemy;
     }
 }
+
+public void Returnenemy(GameObject enemy){
+  enemy.SetActive(false);
+  enemypool.Enqueue(enemy);
+ }
+
+}
+
+
+
